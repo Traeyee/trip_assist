a3f04c5af0ce39f271e97e343f18d970d57b04f9
Sosuke <397305905@qq.com>
2016-05-31 16:35:47 +0800

    a

--- start.cpp
+++ start.cpp
@@ -6,6 +6,7 @@
 #include "map.h"
 #include "config.h"
 #include <cstdio>
+#include <QPainter>
 
 start::start(Block* blocks, int r, int c, int n, QWidget *parent) :
     QWidget(parent),
@@ -16,19 +17,20 @@ start::start(Block* blocks, int r, int c, int n, QWidget *parent) :
     column = c;
     cityNum = n;
     int i, j;
-    ui->gridLayout->setSpacing(30);
+    ui->gridLayout->setSpacing(0);
     for(i = 0; i < row; i ++)
-	for(j = 0; j < column; j ++)
-	    ui->gridLayout->addWidget(blocks + i * row + j, i, j);
+    for(j = 0; j < column; j ++)
+        ui->gridLayout->addWidget(blocks + i * row + j, i, j);
 //强制设置窗体大小
 //    setLayout(mainLayout);
-    //  setFixedSize(850,700);
+  //setFixedSize(510,450);
 
     //建立时间
     sysTime.setHMS(6, 0, 0);
     QTimer* timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(timeUpdate()));
     timer->start(1000);
+    //ui->statusBar->showMessage(tr("Coded by sosuke"));
 }
 
 start::~start()
@@ -46,4 +48,20 @@ void start::timeUpdate()
 {
     sysTime = sysTime.addSecs(TIMEUNIT);
     ui->sysTimeUi->setText(sysTime.toString("HH:mm"));
+}
+
+void start::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    //设置一个矩形
+    QRectF rect(10, 10, 490, 401);
+    painter.drawRect(rect);
+   // painter.setPen(QColor(Qt::red));
+   // painter.drawText(rect, Qt::AlignHCenter, "yafeilinux");
+
+    QPixmap pix;
+    pix.load("../painter_1/piano.png");
+    painter.drawPixmap(800, 800, 500, 500, pix);
+    painter.setPen(QColor(Qt::red));
+    painter.drawText(410,80,"开启新的旅程吧！");
 }
